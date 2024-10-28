891f5a459d3785cf595a045fd61c7b825c2ce021
harikdava <hardik1901dava@gmail.com>
2024-10-28 10:21:15 +0100

    bug in metrics

--- appereance_model_train.py
+++ appereance_model_train.py
@@ -1,65 +1,53 @@
 import json
+import os
+from uuid import uuid4
+os.environ["TQDM_DISABLE"] = "False"
+
+import time
 from pathlib import Path
-from tqdm import tqdm
+from supervision.utils.file import read_json_file, save_json_file
 import subprocess
-from internal.utils.colmap import read_images_binary
 
 
 def generate_appearance_group(workspace_dir: Path):
-    images_bin_path =  workspace_dir / "sparse"/ "0"/ "images.bin"
-
-    images = read_images_binary(images_bin_path)
+    camera_json_file = workspace_dir / "ns" / "transforms.json"
+    cameras = read_json_file(file_path=camera_json_file)
     image_group = {}
-    for i in tqdm(images, desc="reading image information"):
-        image = images[i]
-        key = image.name
+    for cam in cameras["frames"]:
+        image_file = Path(cam["file_path"])
+        key = image_file.name
         if key not in image_group:
             image_group[key] = []
-        image_group[key].append(image.name)
+        image_group[key].append(image_file.name)
 
     for i in image_group:
         image_group[i].sort()
 
-    save_path =  workspace_dir / "appearance_groups.json"
+    save_path = workspace_dir / "appearance_groups.json"
     with open(save_path, "w") as f:
         json.dump(image_group, f, indent=4, ensure_ascii=False)
-    print(save_path)
 
 def run_train(workspace_dir: Path):
     dry_run = False
     config_path = "sh_view_dependent.yaml"
-    iterations = 30000
+    iterations = 1000
+    model_version = str(uuid4())
     training_args = [
                         "python", "main.py", "fit",
-                        "--data.parser", "Colmap",
                         "--config", config_path,
+                        "--data.parser", "NGP",
                         "--data.path", str(workspace_dir),
                         "--data.parser.appearance_groups", "appearance_groups",
                         "--model.renderer.init_args.optimization.max_steps", str(iterations),
                         "--trainer.limit_val_batches", "0",
                         "--save_iterations", f"[30_000, {iterations}]",
                         "--float32_matmul_precision", "highest",
-                        #"--model.density.cull_opacity_threshold", "0.001",
+                        "-v", model_version
                     ]
                     
     print("***************************")
     print(training_args)
     print("***************************")
-
-    _training_args = [
-                        "python", "main.py", "fit",
-                        "--data.parser", "Colmap",
-                        "--config", config_path,
-                        "--data.path", str(workspace_dir),
-                        "--data.parser.appearance_groups", "appearance_groups",
-                        "--model.renderer.init_args.optimization.max_steps", str(iterations),
-                        "--trainer.limit_val_batches", "0",
-                        #"--model.density.absgrad", "true",
-                        "--model.density.cull_opacity_threshold", "0.0009",
-                        "--model.density.densify_grad_threshold", "0.0002",
-                        "-v", "high_app_low_opa"
-                        
-                    ]
 
     if dry_run is False:
         subprocess.run(training_args)
@@ -102,14 +90,17 @@ def detect_last_ckpt(ckpt_dir: Path):
             last_iteration = int(iteration)
     return last_ckpt
 
-projects =    ["/data set name here"]
+projects =    ["cactus"]
 
 for project in projects:
+    start_time = time.time()
     workspace = Path(f"data/{project}")
     generate_appearance_group(workspace_dir=workspace)
-    run_train(workspace_dir=workspace)
-    model_dir = Path(f"outputs/data_{project}/checkpoints/")
+    model_version = run_train(workspace_dir=workspace)
+    model_dir = Path(f"outputs/data_{project}/{model_version}/checkpoints/")
     last_ckpt_file = detect_last_ckpt(ckpt_dir=model_dir)
     fuse_ckpt(model_path=last_ckpt_file, data_path=workspace)
     fused_ckpt = model_dir / "fused.ckpt"
     convert_to_ply(ckpt_path=fused_ckpt, ply_path=workspace / f"{project}.ply")
+    end_time = time.time()
+    print("Time taken: ", end_time - start_time)
--- internal/renderers/gsplat_hit_pixel_count_renderer.py
+++ internal/renderers/gsplat_hit_pixel_count_renderer.py
@@ -1,8 +1,12 @@
 from typing import Optional
 import torch
 from .renderer import Renderer
-from .gsplat_renderer import GSPlatRenderer
 from gsplat_light.hit_pixel_count import hit_pixel_count
+from gsplat_light import project_gaussians
+
+
+DEFAULT_BLOCK_SIZE: int = 16
+DEFAULT_ANTI_ALIASED_STATUS: bool = True
 
 
 class GSplatHitPixelCountRenderer(Renderer):
@@ -18,7 +22,7 @@ class GSplatHitPixelCountRenderer(Renderer):
             block_size: int = 16,
             extra_projection_kwargs: dict = None,
     ):
-        xys, depths, radii, conics, comp, num_tiles_hit, cov3d = GSPlatRenderer.project(
+        xys, depths, radii, conics, comp, num_tiles_hit, cov3d = GSplatHitPixelCountRenderer.project(
             means3D=means3D,
             scales=scales,
             rotations=rotations,
@@ -44,3 +48,33 @@ class GSplatHitPixelCountRenderer(Renderer):
         )
 
         return count, opacity_score, alpha_score, visibility_score
+
+    @staticmethod
+    def project(
+            means3D: torch.Tensor,  # xyz
+            scales: Optional[torch.Tensor],
+            rotations: Optional[torch.Tensor],  # remember to normalize them yourself
+            viewpoint_camera,
+            scaling_modifier=1.0,
+            block_size: int = DEFAULT_BLOCK_SIZE,
+            extra_projection_kwargs: dict = None,
+    ):
+        img_height = int(viewpoint_camera.height.item())
+        img_width = int(viewpoint_camera.width.item())
+
+        return project_gaussians(  # type: ignore
+            means3d=means3D,
+            scales=scales,
+            glob_scale=scaling_modifier,
+            quats=rotations,
+            viewmat=viewpoint_camera.world_to_camera.T[:3, :],
+            # projmat=viewpoint_camera.full_projection.T,
+            fx=viewpoint_camera.fx.item(),
+            fy=viewpoint_camera.fy.item(),
+            cx=viewpoint_camera.cx.item(),
+            cy=viewpoint_camera.cy.item(),
+            img_height=img_height,
+            img_width=img_width,
+            block_width=block_size,
+            **({} if extra_projection_kwargs is None else extra_projection_kwargs),
+        )
